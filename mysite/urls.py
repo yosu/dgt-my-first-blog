56d4dfd64f6a0c1c347ebca11877fff6e8a151e6
yosu <woodstock830@gmail.com>
2018-06-27 17:06:41 +0900

    Add login page

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,10 @@ from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
 
+from django.contrib.auth import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', views.login, name='login'),
     url(r'', include('blog.urls')),
 ]
